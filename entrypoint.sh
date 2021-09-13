4e8f0bc9ff5f811711b027e1dffd37021e86f344
waytorail <86863730+waytorail@users.noreply.github.com>
2021-09-13 13:22:59 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-jupyter notebook --ip=0.0.0.0 --port=$PORT --NotebookApp.token='' --NotebookApp.password=''
+jupyter notebook --ip=0.0.0.0 --port=$PORT --NotebookApp.token='' --NotebookApp.password='' --allow-root
